60247bea617ca780bd05e4cc4d398e091714ef88
1f10230234 <s1f102302348@iniad.org>
2023-05-05 13:19:05 +0900

    minesweeper.b-1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-//import { count } from 'console';
 import { useState } from 'react';
 import styles from './index.module.css';
 
@@ -35,7 +34,7 @@ const Home = () => {
     [-1, 0],
     [-1, 1],
   ];
-  console.log('a');
+  let isDraw = false;
   const board: number[][] = [
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
@@ -63,44 +62,31 @@ const Home = () => {
   };
   ////
   const checkAround = (x: number, y: number) => {
-    //console.log('a');
-    //console.log(y, x);
     board[y][x] = 0;
     for (let k = 0; k < 2; k++) {
-      //console.log('i');
       if (board[y][x] === 0) {
-        //console.log('u');
         for (const d of direction) {
-          //console.log('e');
           if (k) {
-            //console.log('o');
             if (
               board[y + d[1]] != undefined &&
               board[x + d[0]] != undefined &&
               board[y + d[1]][x + d[0]] === -1 &&
               bombMap[y + d[1]][x + d[0]] === 0
             ) {
-              //console.log(y, x);
               checkAround(x + d[0], y + d[1]);
             }
           } else {
-            //console.log('ki');
             if (
               bombMap[y + d[1]] != undefined &&
               bombMap[x + d[0]] != undefined &&
               bombMap[y + d[1]][x + d[0]] === 1
             ) {
-              //console.log('ku');
               board[y][x] += 1;
-              console.log(y, x);
-              //console.log(d);
             }
           }
         }
       }
     }
-    console.log('newboard');
-    console.table(board);
   };
   ////
   const makeBoard = () => {
@@ -114,6 +100,7 @@ const Home = () => {
   };
   ////
   const clickCell = (x: number, y: number) => {
+    isDraw = true;
     if (newUserInputs.some((row: number[]) => row.includes(1))) {
       //
     } else {
@@ -122,26 +109,53 @@ const Home = () => {
       }
     }
     newUserInputs[y][x] = 1;
-    console.log('newUserInputs');
-    console.table(newUserInputs);
-    console.log('bombMap');
-    console.table(bombMap);
-    console.log('board');
-    console.table(board);
+    // console.log('newUserInputs');
+    // console.table(newUserInputs);
+    // console.log('bombMap');
+    // console.table(bombMap);
+    // console.log('board');
+    // console.table(board);
     setUserInputs(newUserInputs);
   };
+  console.log(isDraw);
   makeBoard();
 
-  //console.log('end');
+  if (newUserInputs.some((row: number[]) => row.includes(1))) {
+    let game = 0;
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        if (bombMap[j][i]) {
+          if (board[j][i] === -1) {
+            game += 1;
+          }
+        } else {
+          if (board[j][i] !== -1) {
+            game += 1;
+          }
+        }
+      }
+    }
+    if (game === 81) {
+      setTimeout(function () {
+        alert('your win!');
+      }, 0);
+    }
+    console.log(game);
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.board}>
         {board.map((row: number[], y) =>
           row.map((cell, x) => (
-            <div className={styles.cell} key={`${x}-${y}`} onClick={() => clickCell(x, y)}>
-              {cell === -1 && (
-                <div className={styles.tile} style={{}}>
-                  {cell}
+            <div className={styles.border} key={`${x}-${y}`} onClick={() => clickCell(x, y)}>
+              {cell !== 0 && (
+                <div className={styles.cell} style={{ backgroundPosition: -30 * cell + 30 }}>
+                  {cell === -1 && (
+                    <div className={styles.tile} style={{}}>
+                      {cell}
+                    </div>
+                  )}
                 </div>
               )}
             </div>
