57671eebf45bc55fdad2bc11268359619e46edbc
1f10230234 <s1f102302348@iniad.org>
2023-05-08 07:01:07 +0900

    minesweeperb-2.1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -60,47 +60,48 @@ const Home = () => {
   };
   ////
   function newGame() {
-    if (confirm('新しいゲームを作りますか？')) {
-      setUserInputs([
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-      ]);
-      setBombMap([
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-      ]);
-    }
-    return false;
+    setUserInputs([
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]);
+    setBombMap([
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]);
   }
   ////
   const checkAround = (x: number, y: number) => {
     board[y][x] = 0;
     if (bombMap[y][x]) {
-      board[y][x] = 11;
+      board[y][x] = 111;
       for (let i = 0; i < 9; i++) {
         for (let j = 0; j < 9; j++) {
           if (bombMap[j][i]) {
-            board[j][i] = 11;
+            if (board[j][i] < 100) {
+              board[j][i] = 11;
+            }
           }
         }
       }
     } else {
       for (let k = 0; k < 2; k++) {
-        if (board[y][x] === 0 && userInputs[y][x] !== 10) {
+        if (
+          board[y][x] === 0 //&& newUserInputs[y][x] !== 10
+        ) {
           for (const d of direction) {
             if (k) {
               if (
@@ -109,6 +110,12 @@ const Home = () => {
                 board[y + d[1]][x + d[0]] === -1 &&
                 bombMap[y + d[1]][x + d[0]] === 0
               ) {
+                if (
+                  newUserInputs[y + d[1]][x + d[0]] === 10 ||
+                  newUserInputs[y + d[1]][x + d[0]] === 9
+                ) {
+                  clickCell(x + d[0], y + d[1]);
+                }
                 checkAround(x + d[0], y + d[1]);
               }
             } else {
@@ -124,8 +131,8 @@ const Home = () => {
         }
       }
     }
-    if (userInputs[y][x] === 1 && board[y][x] < 9 && board[y][x] > 0) {
-      console.log('a');
+    if (newUserInputs[y][x] === 1 && board[y][x] < 9 && board[y][x] > 0) {
+      //console.log('a');
       let count = 0;
       for (let k = 0; k < 2; k++) {
         for (const d of direction) {
@@ -135,16 +142,17 @@ const Home = () => {
               board[x + d[0]] != undefined &&
               board[y][x] === count &&
               board[y + d[1]][x + d[0]] === -1 &&
-              userInputs[y + d[1]][x + d[0]] !== 10
+              newUserInputs[y + d[1]][x + d[0]] !== 10
             ) {
               console.log(count);
+              //newUserInputs[y + d[1]][x + d[0]] = 1;
               checkAround(x + d[0], y + d[1]);
             }
           } else {
             if (
               bombMap[y + d[1]] != undefined &&
               bombMap[x + d[0]] != undefined &&
-              userInputs[y + d[1]][x + d[0]] === 10
+              newUserInputs[y + d[1]][x + d[0]] === 10
             ) {
               count += 1;
               console.log(count);
@@ -160,61 +168,64 @@ const Home = () => {
       for (let j = 0; j < 9; j++) {
         if (newUserInputs[j][i] === 1) {
           checkAround(i, j);
-        } else if (userInputs[j][i] !== 0) {
-          board[j][i] = userInputs[j][i];
+        }
+      }
+    }
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        if (newUserInputs[j][i] > 2) {
+          board[j][i] = newUserInputs[j][i];
         }
       }
     }
   };
-  ////
+  //右クリック時の挙動
   const rClickCell = (x: number, y: number) => {
-    //console.log('aa');
     if (board[y][x] === -1) {
       newUserInputs[y][x] = 10;
-      //console.log('ii');
     } else if (board[y][x] === 10) {
       newUserInputs[y][x] = 9;
-      //console.log('uu');
     } else if (board[y][x] === 9) {
       newUserInputs[y][x] = 0;
-      //console.log('ee');
     }
     setUserInputs(newUserInputs);
+    document.getElementsByTagName('html')[0].oncontextmenu = function () {
+      return false;
+    };
   };
-  ////
+  //左クリック時の挙動
   const clickCell = (x: number, y: number) => {
-    if (newUserInputs.some((row: number[]) => row.includes(1))) {
-      //
-    } else {
-      bombMap[y][x] = 1;
-      for (const d of direction) {
-        if (bombMap[y + d[1]] != undefined && bombMap[x + d[0]] != undefined) {
-          bombMap[y + d[1]][x + d[0]] = 1;
+    console.log(game);
+    if (game > -1 && game < 82) {
+      if (newUserInputs.some((row: number[]) => row.includes(1))) {
+        //
+      } else {
+        //bomb配置
+        bombMap[y][x] = 1;
+        for (const d of direction) {
+          if (bombMap[y + d[1]] != undefined && bombMap[x + d[0]] != undefined) {
+            bombMap[y + d[1]][x + d[0]] = 1;
+          }
+        }
+        for (let b = 0; b < 10; b++) {
+          setBombRandom(x, y);
+        }
+        bombMap[y][x] = 0;
+        for (const d of direction) {
+          if (bombMap[y + d[1]] != undefined && bombMap[x + d[0]] != undefined) {
+            bombMap[y + d[1]][x + d[0]] = 0;
+          }
         }
       }
-      for (let b = 0; b < 10; b++) {
-        setBombRandom(x, y);
+      if (newUserInputs[y][x] !== 10) {
+        newUserInputs[y][x] = 1;
       }
-      bombMap[y][x] = 0;
-      for (const d of direction) {
-        if (bombMap[y + d[1]] != undefined && bombMap[x + d[0]] != undefined) {
-          bombMap[y + d[1]][x + d[0]] = 0;
-        }
-      }
+      setUserInputs(newUserInputs);
     }
-    if (userInputs[y][x] !== 10) {
-      newUserInputs[y][x] = 1;
-    }
-    // console.log('newUserInputs');
-    // console.table(newUserInputs);
-    // console.log('bombMap');
-    // console.table(bombMap);
-    // console.log('board');
-    // console.table(board);
-    setUserInputs(newUserInputs);
   };
   makeBoard();
   let game = 0;
+  //ゲームの状態判断
   if (newUserInputs.some((row: number[]) => row.includes(1))) {
     for (let i = 0; i < 9; i++) {
       for (let j = 0; j < 9; j++) {
@@ -222,67 +233,63 @@ const Home = () => {
           if (board[j][i] === -1 || board[j][i] === 10) {
             game += 1;
           }
-          if (userInputs[j][i] === 1) {
+          if (newUserInputs[j][i] === 1) {
             game = -999;
-            // setTimeout(function () {
-            //   alert('You lose...');
-            //   newGame();
-            // }, 0);
           }
         } else {
-          if (board[j][i] !== -1 && board[j][i] !== 10) {
+          if (board[j][i] !== -1 && board[j][i] !== 10 && board[j][i] !== 9) {
             game += 1;
           }
         }
       }
     }
-    if (game === 81) {
-      // setTimeout(function () {
-      //   alert('You won!');
-      //   newGame();
-      // }, 0);
-    }
-    console.log(game);
-    document.getElementsByTagName('html')[0].oncontextmenu = function () {
-      return false;
-    };
   }
 
   return (
     <div className={styles.container}>
-      <div
-        className={styles.nikkori}
-        style={{ backgroundPosition: game === 81 ? 60 : game < 0 ? 30 : 90 }}
-      />
-
-      <div className={styles.board}>
-        {board.map((row: number[], y) =>
-          row.map((cell, x) => (
-            <div
-              className={styles.border}
-              key={`${x}-${y}`}
-              onClick={() => clickCell(x, y)}
-              onContextMenu={() => rClickCell(x, y)}
-            >
-              {cell !== 0 && (
-                <div className={styles.cell} style={{ backgroundPosition: -30 * cell + 30 }}>
-                  {(cell === -1 || cell === 9 || cell === 10) && (
-                    <div className={styles.tile} style={{}}>
-                      {(cell === 9 || cell === 10) && (
-                        <div
-                          className={styles.flag}
-                          style={{
-                            backgroundPosition: -30 * cell + 30,
-                          }}
-                        />
-                      )}
-                    </div>
-                  )}
-                </div>
-              )}
-            </div>
-          ))
-        )}
+      <div className={styles.menuBoard} style={{}}>
+        <div className={styles.nikkoriTile} style={{}}>
+          <div
+            className={styles.nikkori}
+            style={{ backgroundPosition: game === 81 ? 60 : game < 0 ? 30 : 90 }}
+            onClick={() => newGame()}
+          />
+        </div>
+      </div>
+      <div className={styles.containerBorder}>
+        <div className={styles.board}>
+          {board.map((row: number[], y) =>
+            row.map((cell, x) => (
+              <div
+                className={styles.border}
+                key={`${x}-${y}`}
+                onClick={() => clickCell(x, y)}
+                onContextMenu={() => rClickCell(x, y)}
+                style={{ backgroundColor: cell > 100 ? '#f00' : '#bbb' }}
+              >
+                {cell !== 0 && (
+                  <div
+                    className={styles.cell}
+                    style={{ backgroundPosition: -30 * (cell % 100) + 30 }}
+                  >
+                    {(cell % 100 === -1 || cell % 100 === 9 || cell % 100 === 10) && (
+                      <div className={styles.tile} style={{}}>
+                        {(cell % 100 === 9 || cell % 100 === 10) && (
+                          <div
+                            className={styles.flag}
+                            style={{
+                              backgroundPosition: -30 * (cell % 100) + 30,
+                            }}
+                          />
+                        )}
+                      </div>
+                    )}
+                  </div>
+                )}
+              </div>
+            ))
+          )}
+        </div>
       </div>
     </div>
   );
