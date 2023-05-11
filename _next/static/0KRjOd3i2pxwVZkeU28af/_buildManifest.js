2249ce698e6c22ccf5885cc3cd454dcaf9e18faa
1f10230234 <1f10230234@users.noreply.github.com>
2023-05-11 03:05:29 +0000

    deploy: 5d52e1a0af6cfa592436072cb23220d23117356a

---
+++ _next/static/0KRjOd3i2pxwVZkeU28af/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e9ca5c9d08e575a7.css","static/chunks/pages/index-7208d2dd1132651e.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
